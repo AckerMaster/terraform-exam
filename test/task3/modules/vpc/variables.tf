1ae304d4620b4e2f95575cad85f821bb300da53c
liad <liad@gmail.com>
2025-02-08 13:56:00 +0200

    added variables in the vpc module

---
+++ test/task3/modules/vpc/variables.tf
@@ -0,0 +1,16 @@
+
+
+variable "vpc_name" {}
+
+variable "vpc_cidr" {}
+
+variable "public_subnet_count" {}
+
+variable "private_subnet_count" {}
+
+variable "public_subnet_cidr" {}
+
+variable "private_subnet_cidr" {}
+
+variable "availability_zones" {}
+
