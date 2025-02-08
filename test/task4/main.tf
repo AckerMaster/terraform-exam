9389cb6ebef4c3b92190a6832ab4f29043947062
liad <liad@gmail.com>
2025-02-08 22:27:21 +0200

    added settings to autoscaling module in main file

--- test/task4/main.tf
+++ test/task4/main.tf
@@ -15,3 +15,18 @@ module "alb" {
     public_subnet_id  = var.public_subnet_ids
   
 }
+
+module "autoscaling" {
+    source              = "./modules/autoscaling"
+    launch_template_name = "liad-launch-template"
+    ami_id              = var.ami_id
+    instance_type       = var.instance_type
+    auto_scaling_group_name            = "liad-auto-scaling-group"
+    min_size            = 1
+    max_size            = 3
+    desired_capacity    = 1
+    public_subnet_ids   = var.public_subnet_ids
+    security_group_id   = var.security_group_id
+    target_group_arn    = module.alb.target_group_arn
+    instance_name       = "liad-instance"
+}
