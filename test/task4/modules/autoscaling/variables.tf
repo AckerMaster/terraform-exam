1475aba5519a935472ec1603bb2cef93e528735f
liad <liad@gmail.com>
2025-02-08 21:53:17 +0200

    added more variables for autoscaling mosule

--- test/task4/modules/autoscaling/variables.tf
+++ test/task4/modules/autoscaling/variables.tf
@@ -13,3 +13,24 @@ variable "security_group_id" {}
 
 # the instance name
 variable "instance_name" {}
+
+# the auto scaling group name
+variable "auto_scaling_group_name" {}
+
+# minimum number of instances for auto scaling
+variable "min_size" {}
+
+# maximum number of instances for auto scaling
+variable "max_size" {}
+
+# desired number of instances for auto scaling
+variable "desired_capacity" {}
+
+# public subnet ids
+variable "public_subnet_ids" {}
+
+# security group id
+variable "security_group_id" {}
+
+# the target group ARN
+variable "target_group_arn" {}
