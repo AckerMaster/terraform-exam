747e5b4e7dae19a4e9ffd43662abcce10adf6d38
liad <liad@gmail.com>
2025-02-08 21:46:50 +0200

    added variables to autoscaling module

---
+++ test/task4/modules/autoscaling/variables.tf
@@ -0,0 +1,15 @@
+
+# the launch template name
+variable "launch_template_name" {}
+
+# AMI id
+variable "ami_id" {}
+
+# instance type
+variable "instance_type" {}
+
+# the security group ID
+variable "security_group_id" {}
+
+# the instance name
+variable "instance_name" {}
