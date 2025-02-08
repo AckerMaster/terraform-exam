b65aae3e69b64c6cecc32f656318eca43ff5169e
liad <liad@gmail.com>
2025-02-08 22:25:08 +0200

    added variables

---
+++ test/task4/variables.tf
@@ -0,0 +1,37 @@
+# ----------------------------------------------------#
+# ----------------- choose region --------------------#
+
+# our region
+variable "region" {
+    default = "us-east-1"
+}
+
+# ----------------------------------------------------#
+# ----------- alb related configurations -------------#
+
+#vpc id
+variable "vpc_id" {}
+
+#public subnets ids
+variable "public_subnet_ids" {}
+
+# security group id
+variable "security_group_id" {}
+
+
+
+# ----------------------------------------------------#
+# ----------- autoscaling related configurations -------------#
+
+#ami id for Ubuntu 22.04
+variable "ami_id" {
+    default = "ami-0e1bed4f06a3b463d"
+}
+
+# the instance type
+variable "instance_type" {
+    default = "t2.micro"
+    description = "instance type for our EC2"
+}
+
+# ----------------------------------------------------#
