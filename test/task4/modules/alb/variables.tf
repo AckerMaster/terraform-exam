6a9cab5aa4517ac3eeda0b54ce398c97c0836431
liad <liad@gmail.com>
2025-02-08 21:21:55 +0200

    added variables for alb module

---
+++ test/task4/modules/alb/variables.tf
@@ -0,0 +1,12 @@
+
+# the load balancer name
+variable "alb_name" {}
+
+# the VPC id
+variable "vpc_id" {}
+
+# the security group id
+variable "security_group_id" {}
+
+# the public subnet id
+variable "public_subnet_id" {}
