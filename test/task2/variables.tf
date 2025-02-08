e25c2088cb4df661f5d9b69b8f1c0b55b6bbea43
liad <liad@gmail.com>
2025-02-08 11:44:39 +0200

    added comments

--- test/task2/variables.tf
+++ test/task2/variables.tf
@@ -4,8 +4,18 @@ variable "region" {
     default = "us-east-1"
 }
 
-
+# the instance type
 variable "instance_type" {
     default = "t2.micro"
     description = "instance type for our EC2"
+}
+
+# set user name
+variable "admin_username" {
+    default = "admin-user"
+}
+
+# set password
+variable "admin_password" {
+    default = "Password123!"
 }
