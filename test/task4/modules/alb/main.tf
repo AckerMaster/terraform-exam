6d52b3f4b3be797a2ec1559dd863c6d1d86a0c4e
liad <liad@gmail.com>
2025-02-08 21:28:39 +0200

    added target group resource with health check

--- test/task4/modules/alb/main.tf
+++ test/task4/modules/alb/main.tf
@@ -13,3 +13,25 @@ resource "aws_lb" "alb" {
     }
 
 }
+
+resource "aws_lb_target_group" "target_group" {
+    
+    name      = var.target_group_name
+    port      = 80
+    protocol  = "HTTP"
+    vpc_id    = var.vpc_id
+
+    health_check {
+        
+        path                = "/"
+        interval            = 30
+        timeout             = 5
+        healthy_threshold   = 3
+        unhealthy_threshold = 2
+    }
+
+    tags = {
+        Name = var.target_group_name
+    }
+
+}
